b12a401736b1bce3557bbc0d2d2db1d7acfebbd5
Женя Никода <D:\Users\Nikoda\AppData\Roaming\The Bat!>
2020-06-05 07:31:13 +0300

    Init lab5, added untracked files

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace lab5
+{
+    public class Program
+    {
+
+
+        public static string encrypt(string adress)
+        {
+            string text = File.ReadAllText(adress);
+            string newText = "";
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                int code = (int)text[i];
+                code += 5;
+                newText += (char)code;
+            }
+            return newText;
+        }
+
+        public static string decrypt(string text)
+        {
+            string newText = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                int code = (int)text[i];
+                code -= 5;
+                newText += (char)code;
+            }
+
+            return newText;
+        }
+
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+}
